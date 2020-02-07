a0074cd6653178064d3e2717eb67f1789be767b7
SandeepJoel <joeltheking00000001@gmail.com>
2020-02-07 07:42:17 +0530

    Base animation done

--- src/animations/waves-static-background/script.js
+++ src/animations/waves-static-background/script.js
@@ -1,12 +1,22 @@
-let toggle = false;
+
 setInterval(() => {
-  if (toggle) {
-    document.getElementById('a1').classList.remove('hide')
-    document.getElementById('a2').classList.add('hide')
-    toggle = false;
+  let currentPrimary = document.querySelector('.primary');
+  let nextPrimary = currentPrimary.nextElementSibling;
+  if (nextPrimary) {
+    nextPrimary.classList.add('primary');
+    nextPrimary.classList.remove('hide');
   } else {
-    document.getElementById('a2').classList.remove('hide')
-    document.getElementById('a1').classList.add('hide')
-    toggle = true;
+    document.querySelector('.svg-list').firstElementChild.classList.add('primary');
+    document.querySelector('.svg-list').firstElementChild.classList.remove('hide');
   }
+
+  let currentSecondary = document.querySelector('.secondary');
+  if(currentSecondary) {
+    currentSecondary.classList.remove('secondary');
+    currentSecondary.classList.add('hide');
+  }  
+
+  currentPrimary.classList.remove('primary');
+  currentPrimary.classList.add('secondary');
+
 }, 10000);
