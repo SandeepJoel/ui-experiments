5ef3e2834a81faf841a152f27ed46b18731a5744
SandeepJoel <joeltheking00000001@gmail.com>
2020-02-09 01:00:46 +0530

    Complete automation of waves

--- src/animations/waves-static-background/script.js
+++ src/animations/waves-static-background/script.js
@@ -1,22 +1,92 @@
-
-setInterval(() => {
-  let currentPrimary = document.querySelector('.primary');
-  let nextPrimary = currentPrimary.nextElementSibling;
-  if (nextPrimary) {
-    nextPrimary.classList.add('primary');
-    nextPrimary.classList.remove('hide');
-  } else {
-    document.querySelector('.svg-list').firstElementChild.classList.add('primary');
-    document.querySelector('.svg-list').firstElementChild.classList.remove('hide');
+function setAttributes(el, attrs) {
+  for (var key in attrs) {
+    el.setAttribute(key, attrs[key]);
   }
+}
 
-  let currentSecondary = document.querySelector('.secondary');
-  if(currentSecondary) {
-    currentSecondary.classList.remove('secondary');
-    currentSecondary.classList.add('hide');
-  }  
+function generateRandomString() {
+  return Math.random().toString(36).substring(7);
+}
 
-  currentPrimary.classList.remove('primary');
-  currentPrimary.classList.add('secondary');
+// This function returns svg dom object of the wave based
+// of the passed config object
+function generateWaveHtml(config, svgClasses) {
+  let { type, wave, colors, color } = config;
+  let isGradient = type === 'gradient';
+  let id = generateRandomString();
+  let str = `
+    <svg xmlns="http://www.w3.org/2000/svg" class="${svgClasses} ${wave}" 
+      preserveAspectRatio="none" viewBox="0 0 580 400">
+      <path fill="${isGradient ? `url(#${id})` : color}" />
+        ${isGradient ? `<defs>
+            <linearGradient id="${id}" spreadMethod="pad">
+              <stop offset="0%" stop-color="${colors[0]}" stop-opacity="1" />
+              <stop offset="100%" stop-color="${colors[1]}" stop-opacity="1" />
+            </linearGradient>
+        </defs> `
+      : ''}      
+    </svg> `;
+  
+  return new DOMParser().parseFromString(str,
+    'text/xml'
+  ).firstChild;
+}
 
-}, 10000);
+function init() {
+  const colorsConfig = [
+    {
+      type: 'gradient',
+      colors: ['#4facfe', '#00f2fe'],
+      wave: 'blob1'
+    },
+    {
+      type: 'singleColor',
+      color: '#000',
+      wave: 'blob1'
+    },
+    {
+      type: 'gradient',
+      colors: ['#f093fb', '#f5576c'],
+      wave: 'blob2'
+    },
+    {
+      type: 'singleColor',
+      color: '#FFF',
+      wave: 'blob2'
+    },
+    {
+      type: 'gradient',
+      colors: ['#84fab0', '#8fd3f4'],
+      wave: 'blob1'
+    },
+    {
+      type: 'singleColor',
+      color: '#000',
+      wave: 'blob1'
+    },
+    {
+      type: 'gradient',
+      colors: ['#fa709a', '#fee140'],
+      wave: 'blob1'
+    },
+  ];
+
+  const svgRoot = document.querySelector('.svg-list');
+  svgRoot.appendChild(generateWaveHtml(colorsConfig[0], 'primary'));
+  
+  let index = 1;
+  setInterval(() => {
+    index = index % colorsConfig.length;
+    let currentPrimary = document.querySelector('.primary');
+    svgRoot.appendChild(generateWaveHtml(colorsConfig[index], 'primary'));
+
+    let currentSecondary = document.querySelector('.secondary');
+    if (currentSecondary) {
+      currentSecondary.remove()
+    }
+    currentPrimary.classList.remove('primary');
+    currentPrimary.classList.add('secondary');
+    index++;
+  }, 10000);
+}
+init();
