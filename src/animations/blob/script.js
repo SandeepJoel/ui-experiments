ce9345dde1bb5bfdccfc5b7b8a16b85c70d25252
SandeepJoel <joeltheking00000001@gmail.com>
2020-08-19 23:34:25 +0530

    Final

--- src/animations/blob/script.js
+++ src/animations/blob/script.js
@@ -1,5 +1,19 @@
 import { getRandomNumbersBetween } from '../../utilities/js-helpers.js';
 
+const flatColors = ['#487eb0', '#2ecc71', '#ee5253', '#feca57', '#8c7ae6', '#10ac84', '#2f3640', '#192a56', '#B33771'];
+
+class CssVariableColor {
+  constructor(prop) {
+    this.prop = prop;
+  }
+  get value() {
+    return getComputedStyle(document.documentElement).getPropertyValue(this.prop).trim();
+  }
+  set value(hexString) {
+    document.documentElement.style.setProperty(this.prop, hexString);
+  }
+}
+
 const options = {
   randomize: false,
   position: 'left',
@@ -9,7 +23,9 @@ const options = {
 let timerId;
 
 const gui = new dat.GUI();
-gui.addColor(options, 'backgroundColor').name('Background');
+// gui.addColor(options, 'backgroundColor').name('Background');
+
+let backgroundColor = gui.addColor(new CssVariableColor('--background-color'), 'value').name('Background');
 gui.add(options, 'randomize').name('Randomize').onChange(function() {
   if (options.randomize) {
     randomizeColors();
@@ -17,8 +33,8 @@ gui.add(options, 'randomize').name('Randomize').onChange(function() {
     cancelAnimationFrame(timerId)
   }
 })
-let primaryColor = gui.addColor(options, 'blobColor').name('Blob');
-
+// let primaryColor = gui.addColor(options, 'blobColor').name('Blob');
+let primaryColor = gui.addColor(new CssVariableColor('--blob-color'), 'value').name('Blob');
 var last = Date.now(); // timestamp of the last render() call
 var noOfSeconds = 6;
 var currentTime;
@@ -27,27 +43,18 @@ function randomizeColors() {
   currentTime = Date.now();
   if (currentTime - last >= noOfSeconds * 1000) {
     last = currentTime;
-
     // our work
-    primaryColor.setValue(flatColors[getRandomNumbersBetween(0, flatColors.length - 1)]); 
-    // the above line automatically does this -> options.blobColor = color;
-    setColors();
+    primaryColor.setValue(flatColors[getRandomNumbersBetween(0, flatColors.length - 1)]);     
   }
   timerId = requestAnimationFrame(randomizeColors);
-}
-
-function setColors() {
-  document.querySelector('.box').style.backgroundColor = options.blobColor;
-  [].forEach.call(document.querySelectorAll('.piece'), function (div) {
-    div.style.backgroundColor = options.blobColor;
-  });
 }
 
 function init() {
   if (options.randomize) {
     randomizeColors();
   }
-  setColors();
+  backgroundColor.setValue(options.backgroundColor); 
+  primaryColor.setValue(options.blobColor); 
 }
 
 init();
